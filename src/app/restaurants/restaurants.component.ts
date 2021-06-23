1caf2dce4204a0fc50f880462fcefb0d6323635b
karinaponte <kita_36@hotmail.com>
2021-06-23 13:43:34 -0300

    Adicionando HTTP ao Serviço de Restaurantes

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -13,7 +13,7 @@ export class RestaurantsComponent implements OnInit {
   constructor(private restaurantsService: RestaurantService) { }
 
   ngOnInit() {
-    this.restaurants = this.restaurantsService.restaurants()
+    this.restaurantsService.restaurants()
+      .subscribe(restaurants => this.restaurants = restaurants)
   }
-
 }
