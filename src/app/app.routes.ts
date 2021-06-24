e46a6c58006088082f5d3b49674766014dd91e57
karinaponte <kita_36@hotmail.com>
2021-06-24 16:07:25 -0300

    Criando o Componente de Compra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
+import { OrderComponent } from "./order/order.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
@@ -11,9 +12,10 @@ export const ROUTES: Routes = [
     { path: 'restaurants', component: RestaurantsComponent },
     { path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
-            {path: '', redirectTo: 'menu', pathMatch: 'full'},
-            {path: 'menu', component: MenuComponent},
-            {path: 'reviews', component: ReviewsComponent},
+            { path: '', redirectTo: 'menu', pathMatch: 'full' },
+            { path: 'menu', component: MenuComponent },
+            { path: 'reviews', component: ReviewsComponent },
         ]},
     { path: 'about', component: AboutComponent },
+    { path: 'order', component: OrderComponent }
 ]
